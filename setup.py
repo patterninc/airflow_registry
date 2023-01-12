55124db7706ca1a98e0da60371373ffe8da232f4
wlggraham <wlg.graham@gmail.com>
2023-01-12 09:09:23 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+      name='airflow_registry',
+      version='0.1.0',
+      description='Custom operators, sensors, and hooks to be shared across org',
+      author='Pattern',
+      license='',
+      python_requires='>=3.7.0',
+      packages=find_packages(where='airflow_registry'),
+      package_dir={'': 'airflow_registry'}
+)
