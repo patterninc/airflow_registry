90571ef7d47f451fe2db6e2b4b57672e20e75c39
Jeremy Mumford <jeremy.mumford@pattern.com>
2023-06-27 10:09:35 -0600

    tag matt and jeremy, ignore alice alerts

--- airflow_registry/utils/slack_notifications.py
+++ airflow_registry/utils/slack_notifications.py
@@ -1,12 +1,19 @@
 from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
 
+
 def base_failure_alert(context, conn):
     ti = context.get('task_instance')
     _task = ti.task_id
-    _emoji = ':warning:' if "sensor" in _task else ':alarm:'
-    _message = 'Possible data delay' if "sensor" in _task else 'Unexpected error'
+    _emoji = ':alarm:'
+    _message = 'Unexpected error @Jeremy Mumford @Matt Brown'
+    if "alice" in _task:
+        _emoji = ':taco:'
+        _message = 'Alice currently only running once a day, you can probably ignore this'
+    elif "sensor" in _task:
+        _emoji = ':warning:'
+        _message = 'Possible data delay @Jeremy Mumford @Matt Brown'
     slack_msg = """
-    {emoji} {task} failed in {dag} {emoji}
+    {emoji} {task} failed in *{dag}* {emoji}
     *Execution Time*: {exec_date}
     {message}, investigate at {log_url}
     """.format(
@@ -23,8 +30,10 @@ def base_failure_alert(context, conn):
         message=slack_msg)
     return failed_alert.execute(context=context)
 
+
 def failure_alert(context):
     return base_failure_alert(context, 'slack_failure_alert')
+
 
 def content_failure_alert(context):
     return base_failure_alert(context, 'slack_dev_data_quality_airflow_alerts')
