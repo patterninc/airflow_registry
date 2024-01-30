df59d08f78d3fc68d3f63a2e91e2bbd79b3c340c
wlggraham <wlg.graham@gmail.com>
2024-01-29 17:23:34 -0700

    add snowflake table param

--- airflow_registry/operators/sodaCheckOperator.py
+++ airflow_registry/operators/sodaCheckOperator.py
@@ -1,7 +1,7 @@
 from airflow.models import BaseOperator
 
 class SodaCheckOperator(BaseOperator):
-    def __init__(self, scan_name, config_file, check_function, data_source, project_root, checks_subpath, *args, **kwargs):
+    def __init__(self, scan_name, config_file, check_function, data_source, project_root, checks_subpath=None, snowflake_table=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.scan_name = scan_name
         self.config_file = config_file
@@ -9,6 +9,7 @@ class SodaCheckOperator(BaseOperator):
         self.data_source = data_source
         self.project_root = project_root
         self.checks_subpath = checks_subpath
+        self.snowflake_table = snowflake_table 
 
     def execute(self, context):
-        return self.check_function(scan_name=self.scan_name, config_file=self.config_file, data_source=self.data_source, project_root=self.project_root, checks_subpath=self.checks_subpath, context=context)
+        return self.check_function(scan_name=self.scan_name, config_file=self.config_file, data_source=self.data_source, project_root=self.project_root, checks_subpath=self.checks_subpath, snowflake_table=self.snowflake_table, context=context)
--- airflow_registry/utils/soda_check_function.py
+++ airflow_registry/utils/soda_check_function.py
@@ -18,7 +18,7 @@ class CustomSampler(Sampler):
           "row": row
         })
 
-def check(scan_name, checks_subpath=None, config_file=None, data_source='snowflake', project_root='/usr/local/airflow/include/soda', context=None):
+def check(scan_name, checks_subpath=None, config_file=None, data_source='snowflake', project_root='/usr/local/airflow/include/soda', snowflake_table=None, context=None):
   from soda.scan import Scan
   from snowflake.connector import connect
   import datetime
@@ -32,6 +32,7 @@ def check(scan_name, checks_subpath=None, config_file=None, data_source='snowfla
     data_source: The data source name.
     project_root: The project root directory.
     config_file: Variable containing Soda configuration string.
+    snowflake_table: The name of the Snowflake table to store the results.
 
   Returns:
     int: The result of the scan execution.
@@ -59,45 +60,44 @@ def check(scan_name, checks_subpath=None, config_file=None, data_source='snowfla
   exit_code = scan.execute()
   data = scan.get_scan_results()
   config = json.loads(config_file)
-  summary = scan.get_logs_text().split('Oops!')[1].split('Summary:')[0].strip()
 
- 
-  # Extract the connection details
-  user = config[f'data_source {data_source}']['username']
-  password = config[f'data_source {data_source}']['password']
-  account = config[f'data_source {data_source}']['account']
-  warehouse = config[f'data_source {data_source}']['warehouse']
+  if snowflake_table:
+    # Extract the connection details
+    user = config[f'data_source {data_source}']['username']
+    password = config[f'data_source {data_source}']['password']
+    account = config[f'data_source {data_source}']['account']
+    warehouse = config[f'data_source {data_source}']['warehouse']
+    role = config[f'data_source {data_source}']['role']
 
-  with connect(user=user, password=password, account=account, warehouse=warehouse) as conn:
+    with connect(user=user, password=password, account=account, role=role, warehouse=warehouse) as conn:
 
-    scan_start = datetime.datetime.fromisoformat(data.get('scanStartTimestamp'))
+      scan_start = datetime.datetime.fromisoformat(data.get('scanStartTimestamp'))
 
-    for check in data.get('checks'):
-        name = str(check.get('name'))
-        definition = check.get('definition').replace("'", "''").strip() 
-        table_name = check.get('table').split('.')[-1] 
-        outcome = check.get('outcome')
-        diagnostics = json.dumps(check.get('diagnostics'))
-        fail_objects = [row['row'][-1] for row in scan.sampler.failed_rows if row['check_name'] == name] # Fail objects need to be the last column in the row
-        file_path = check.get('location').get('filePath')
-        load_timestamp = datetime.datetime.now().replace(tzinfo=None)
-        db = check.get('table').split('.')[0]
-        schema = check.get('table').split('.')[1]
-        sql = f"INSERT INTO {db}.{schema}.data_quality_tests (NAME, DEFINITION, TABLE_NAME, OUTCOME, DIAGNOSTICS, SCAN_START, SCAN_NAME, FILE_PATH, FAIL_MESSAGES, LOAD_TIMESTAMP) SELECT '{name}', '{definition}', '{table_name}', '{outcome}', PARSE_JSON('{diagnostics}'), TO_TIMESTAMP_NTZ('{scan_start}'), '{scan_name}', '{file_path}', {fail_objects}, TO_TIMESTAMP_NTZ('{load_timestamp}');"
-        try:
-          cursor = conn.cursor()
-          cursor.execute(sql)
-          conn.commit()
-        except Exception as e:
-           slack_message = f"An error occurred while executing the SQL command: {e}"
-           context['ti'].xcom_push(key='slack_fail_message', value=slack_message)
-        finally:
-          cursor.close()
+      for check in data.get('checks'):
+          name = str(check.get('name'))
+          definition = check.get('definition').replace("'", "''").strip() 
+          table_name = check.get('table')
+          outcome = check.get('outcome')
+          diagnostics = json.dumps(check.get('diagnostics'))
+          fail_objects = [row['row'][-1] for row in scan.sampler.failed_rows if row['check_name'] == name] # Fail objects need to be the last column in the row
+          file_path = check.get('location').get('filePath')
+          load_timestamp = datetime.datetime.now().replace(tzinfo=None)
+          sql = f"INSERT INTO {snowflake_table} (NAME, DEFINITION, TABLE_NAME, OUTCOME, DIAGNOSTICS, SCAN_START, SCAN_NAME, FILE_PATH, FAIL_MESSAGES, LOAD_TIMESTAMP) SELECT '{name}', '{definition}', '{table_name}', '{outcome}', PARSE_JSON('{diagnostics}'), TO_TIMESTAMP_NTZ('{scan_start}'), '{scan_name}', '{file_path}', {fail_objects}, TO_TIMESTAMP_NTZ('{load_timestamp}');"
+          try:
+            cursor = conn.cursor()
+            cursor.execute(sql)
+            conn.commit()
+          except Exception as e:
+            slack_message = f"An error occurred while executing the SQL command: {e}"
+            context['ti'].xcom_push(key='slack_fail_message', value=slack_message)
+          finally:
+            cursor.close()
   
   if exit_code != 0:
-    slack_message = f'*Scan Name:* {scan_name}\n    *Results:* {summary}\n\n'
+    slack_message += f'*Scan Name:* {scan_name}\n'
     if scan.has_error_logs():
-      slack_message += f'*Scan Error Logs* \n\n {scan.get_error_logs_text()}\n\n\n'
+      summary = scan.get_logs_text().split('Oops!')[1].split('Summary:')[0].strip()
+      slack_message += f'*Results:* {summary}\n\n*Scan Error Logs* \n\n {scan.get_error_logs_text()}\n\n\n'
     if scan.has_check_fails():
       slack_message += f'*Failed Data Quality Checks:*\n\n'
       for failed_check in scan.get_checks_fail():
