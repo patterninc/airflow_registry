ccf233699a9378dcffd9ef827407e9c7022cb496
wlggraham <wlg.graham@gmail.com>
2024-06-18 15:34:30 -0600

    add dqc slack notification

--- airflow_registry/utils/slack_notifications.py
+++ airflow_registry/utils/slack_notifications.py
@@ -1,4 +1,5 @@
 from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
+from airflow.providers.slack.operators.slack import SlackAPIPostOperator
 
 
 def base_failure_alert(context, conn):
@@ -63,4 +64,30 @@ def custom_failure_alert(context, conn, xcom_key):
         task_id='slack_failure_alert',
         slack_webhook_conn_id=conn,
         message=slack_msg)
+    return failed_alert.execute(context=context)
+
+
+def dqc_failure_alert(context, channel, conn, xcom_key):
+    ti = context.get('task_instance')
+    _task = ti.task_id
+    message = ti.xcom_pull(task_ids=_task, key=xcom_key)
+    _emoji = ':x:'
+    slack_msg = """
+    {emoji} Checks Failed {emoji}
+    *Execution Time:* {exec_date}
+    <{log_url}|*Logs*>
+    {message}
+    """.format(
+        emoji=_emoji,
+        message=message,
+        exec_date=context.get('execution_date'),
+        log_url=ti.log_url)
+
+    failed_alert = SlackAPIPostOperator(
+            task_id='slack_message',
+            slack_conn_id=conn,
+            channel=channel,
+            text=slack_msg,
+            username=''
+        )
     return failed_alert.execute(context=context)
