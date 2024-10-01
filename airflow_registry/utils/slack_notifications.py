ed7d590d797e7bb50f112a35457df7e4243e0bd1
vatadepalli <vatadepalli@hotmail.com>
2024-10-01 18:07:14 +0530

    add env and message optional params

--- airflow_registry/utils/slack_notifications.py
+++ airflow_registry/utils/slack_notifications.py
@@ -1,12 +1,16 @@
 from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
 from airflow.providers.slack.operators.slack import SlackAPIPostOperator
+from typing import Optional
 
 
-def base_failure_alert(context, conn):
+def base_failure_alert(
+    context, conn, environment: Optional[str] = None, message: Optional[str] = None
+):
     ti = context.get("task_instance")
     _task = ti.task_id
+    _environment = f"Environment: *{environment.upper()}* - " if environment else "\b"
     _emoji = ":alarm:"
-    _message = "Unexpected error"
+    _message = message if message else "Unexpected error"
     if "alice" in _task:
         _emoji = ":taco:"
         _message = (
@@ -16,7 +20,7 @@ def base_failure_alert(context, conn):
         _emoji = ":warning:"
         _message = "Possible data delay"
     slack_msg = """
-    {emoji} {task} failed in *{dag}* {emoji}
+    {emoji} {environment} {task} failed in *{dag}* {emoji}
     *Execution Time*: {exec_date}
     {message}, investigate at {log_url}
     """.format(
@@ -24,6 +28,7 @@ def base_failure_alert(context, conn):
         task=_task,
         emoji=_emoji,
         message=_message,
+        environment=_environment,
         exec_date=context.get("execution_date"),
         log_url=ti.log_url,
     )
@@ -34,8 +39,12 @@ def base_failure_alert(context, conn):
     return failed_alert.execute(context=context)
 
 
-def failure_alert(context):
-    return base_failure_alert(context, "slack_failure_alert")
+def failure_alert(
+    context, environment: Optional[str] = None, message: Optional[str] = None
+):
+    return base_failure_alert(
+        context, "slack_failure_alert", environment=environment, message=message
+    )
 
 
 def content_failure_alert(context):
@@ -70,10 +79,10 @@ def custom_failure_alert(context, conn, xcom_key):
 
 
 def dqc_failure_alert(context, channel, conn, xcom_key):
-    ti = context.get('task_instance')
+    ti = context.get("task_instance")
     _task = ti.task_id
     message = ti.xcom_pull(task_ids=_task, key=xcom_key)
-    _emoji = ':x:'
+    _emoji = ":x:"
     slack_msg = """
     {emoji} Checks Failed {emoji}
     *Execution Time:* {exec_date}
@@ -82,14 +91,15 @@ def dqc_failure_alert(context, channel, conn, xcom_key):
     """.format(
         emoji=_emoji,
         message=message,
-        exec_date=context.get('execution_date'),
-        log_url=ti.log_url)
+        exec_date=context.get("execution_date"),
+        log_url=ti.log_url,
+    )
 
     failed_alert = SlackAPIPostOperator(
-            task_id='slack_message',
-            slack_conn_id=conn,
-            channel=channel,
-            text=slack_msg,
-            username=''
-        )
+        task_id="slack_message",
+        slack_conn_id=conn,
+        channel=channel,
+        text=slack_msg,
+        username="",
+    )
     return failed_alert.execute(context=context)
